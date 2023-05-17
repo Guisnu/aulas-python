19a4968bf07330edc2bbe8d73218a819eaf4f7d3
Guisnu <guilhermenunes08925@gmail.com>
2023-05-17 13:42:14 -0300

    vetores

---
+++ as 4/conceito de vetor 1.py
@@ -0,0 +1,6 @@
+#podemos adicionar todos os tipos de dados a lista
+lista =[1,"Guilherme",10.1]
+
+print(lista[2])
+
+
---
+++ as 4/conceito de vetor.py
@@ -0,0 +1,7 @@
+#conceito de vetor
+
+idade = [85,36,12,4]
+
+print(idade[0])
+print(idade[1])
+
