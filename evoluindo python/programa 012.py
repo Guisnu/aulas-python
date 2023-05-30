48097132f3b5c18bb20a06e6329ef610e63ac5cb
Guisnu <guilhermenunes08925@gmail.com>
2023-05-30 20:59:54 -0300

    evoluindo python

---
+++ evoluindo python/programa 012.py
@@ -0,0 +1,6 @@
+#Estrutura condicional parte 1
+
+if(False): 
+    print("código passou por aqui!")
+
+#o código que está dentro de if só é executado se for True(verdadeiro)
---
+++ evoluindo python/programa 013.py
@@ -0,0 +1,12 @@
+#Estrutura condicional parte 2
+
+idade = int(input("Digite a idade: "))
+
+if(idade >= 18):
+    print("Essa pessoa é maior de idade")
+else:
+    print("Essa pessoa é menor de idade")
+
+#se(if) a idade for maior ou igual a 18 será executado "Essa pessoa é maior de idade"
+
+#se não(else) será executado "Essa pessoa é menor de idade"
